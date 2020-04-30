b6bef732cff2625e0741038855d389f73dd35c05
AAA530 <dhairyapatelad@gmail.com>
2020-04-30 16:23:50 +0530

    automated follow

--- bot.py
+++ bot.py
@@ -19,17 +19,30 @@ class InstagramBot:
 
         self.login()
 
+        self.base_url = 'https://www.instagram.com'
+
     def login(self):
-            self.driver.get('https://www.instagram.com')
-            time.sleep(3) #wait till site gets load
+        self.driver.get('https://www.instagram.com')
+        time.sleep(3) #wait till site gets load
 
-            self.driver.find_element_by_name('username').send_keys(self.username)
+        self.driver.find_element_by_name('username').send_keys(self.username)
 
-            self.driver.find_element_by_name('password').send_keys(self.password)
+        self.driver.find_element_by_name('password').send_keys(self.password)
+        self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[4]/button').click()
 
-            self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[4]/button').click()
+        time.sleep(2)
 
+    def nav_user(self,user):
+        self.driver.get('{}/{}/'.format(self.base_url,user))
+
+    def follow_user(self,user):
+        self.nav_user(user)
+
+        follow_button = self.driver.find_element_by_xpath("//button[contains(text(), 'Follow')]")
+        
+        follow_button.click()
 
 
 if __name__ == "__main__":
-    ig_bot = InstagramBot('temp','password123')
+    ig_bot = InstagramBot('user','password')
+    ig_bot.follow_user('gigihadid')
